a9ff1bc8360ca4f5b376f0e89b3bc1c465fd1ed4
Isidora Djordjevic <68898702+IsidoraDjordjevic@users.noreply.github.com>
2021-05-13 10:03:33 +0200

    Add files via upload

---
+++ 30Hills.js
@@ -0,0 +1,35 @@
+let dropdown = document.getElementById('myData');
+dropdown.length = 0;
+
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Friends';
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+const url = 'data.json';
+
+fetch(url)  
+  .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.warn('Looks like there was a problem. Status Code: ' + 
+          response.status);  
+        return;  
+      }
+
+ 
+      response.json().then(function(data) {  
+        let option;
+    
+    	for (let i = 0; i < data.length; i++) {
+          option = document.createElement('option');
+      	  option.text = data[i].firstName;
+      	  dropdown.add(option);
+    	}    
+      });  
+    }  
+  )  
+  .catch(function(err) {  
+    console.error('Fetch Error -', err);  
+  });
